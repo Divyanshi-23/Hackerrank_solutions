d94d13131295817e7799d96fa02262213c37a37a
Divyanshi-23 <143104948+Divyanshi-23@users.noreply.github.com>
2024-04-10 23:21:41 +0530

    Create Output_Formating.java

---
+++ Output_Formating.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.next();
+        int input = sc.nextInt();
+        
+        System.out.println("================================");
+        System.out.printf("%-15s%03d\n",inputString,input);
+        inputString = sc.next();
+        input = sc.nextInt();
+        System.out.printf("%-15s%03d\n",inputString,input);
+        inputString = sc.next();
+        input = sc.nextInt();
+        System.out.printf("%-15s%03d\n",inputString,input);
+        System.out.println("================================");
+        
+        
+    }
+}    
