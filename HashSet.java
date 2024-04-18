5ec005d606ea2004189c55751b384a359cc26eef
Divyanshi-23 <143104948+Divyanshi-23@users.noreply.github.com>
2024-04-18 23:46:08 +0530

    Update and rename HashSet.java to DataStructures/HashSet.java

--- HashSet.java
+++
@@ -1,9 +0,0 @@
-
-    
-        HashSet<String> set= new HashSet<>();
-        String str = null;
-        for(int i=0; i<t;i++){
-            str = pair_left[i].concat(" "+pair_right[i]);
-            set.add(str);
-            System.out.println(set.size());
-        }
